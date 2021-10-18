21de53d8c306649873fc8101f68270c7b8b4c823
Andrew <winger.andrew@gmail.com>
2021-10-18 14:47:04 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,13 @@ get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
+end
+
+post '/signup' do
+  params.to_s
+end
+
