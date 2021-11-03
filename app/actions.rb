5c43c418174a4ff3f60090d9775c223428f64226
Andrew <winger.andrew@gmail.com>
2021-11-03 01:45:02 +0000

    W6D1

--- app/actions.rb
+++ app/actions.rb
@@ -75,4 +75,23 @@ get '/finstagram_posts/:id' do
   erb(:"finstagram_posts/show")
 end
 
+post '/comments' do
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  comment.save
+  redirect(back)
+end
 
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,6 @@ class Like < ActiveRecord::Base
 
     belongs_to :user
     belongs_to :finstagram_post
+    validates_presence_of :user, :finstagram_post
   
   end
-
-  
